e7f2f2542ecc369046607acf10f1b334672cbc7e
TK <tkosudi@gmail.com>
2024-06-08 15:00:47 -0300

    feat(variables): formatted output

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 
 	fmt.Println("Get your tickets he to attend")
 
